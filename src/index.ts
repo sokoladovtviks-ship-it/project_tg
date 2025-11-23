90822d9dfcb6ec225710565fdba2fa7ebd8b2de5
sokoladovtviks-ship-it <sokoladovtviks@gmail.com>
2025-11-23 03:40:22 +0300

    Added index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export const config = {
+  supabase: {
+    url: 'https://jzbonjduchsimnujrecg.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6Ym9uamR1Y2hzaW1udWpyZWNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3NTgwOTMsImV4cCI6MjA3OTMzNDA5M30.GK2qTkiP0-BA9dgeKXvIfc-hGNNmLIN_YuAtWBqB-Fw'
+  },
+  telegram: {
+    botToken: '8136408332:AAGmc89B17B1PQ06nKMeVBx-t5vWBRRnp5U'
+  }
+};
